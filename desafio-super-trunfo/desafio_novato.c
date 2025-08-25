3841754e06cfb29119af38d9384f1c756795675f
Sávio Miranda <saviomrocha7@gmail.com>
2025-08-25 09:29:18 +0000

    declaracao das variaveis

--- desafio-super-trunfo/desafio_novato.c
+++ desafio-super-trunfo/desafio_novato.c
@@ -3,4 +3,12 @@
 int main() {
     
 }
-// 25/08
+// Declaração das variáveis para a carta 1
+char estado1,codigo1[4], nome_cidade1[100];
+int populacao1, pontos_turisticos1;
+float area1, pib1;
+
+// Declafração das variáveis para a carta 2
+char estado2, codigo2[4], nome_cidade2[100];
+int populacao2, pontos_turisticos2;
+float area2, pib2;
