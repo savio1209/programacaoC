b0e60af47413290505aa34676beca15686588d30
Sávio Miranda <saviomrocha7@gmail.com>
2025-09-23 11:17:56 -0300

    Update desafio_novato.c
    Correção na declaração e cálculo das variáveis de densidade populacional e PIB per capita.

--- desafio-super-trunfo/desafio_novato.c
+++ desafio-super-trunfo/desafio_novato.c
@@ -6,21 +6,17 @@ int main() {
 char estado1, codigo1[4], nome_cidade1[100];
 int populacao1, pontos_turisticos1;
 float area1, pib1, densi_pop1, pibpc1; // Acréscimo das variáveis densi_pop1 e pibpc1
-densi_pop1 = populacao1 / area1; // Cálculo da densidade populacional da primeira carta
-pibpc1 = pib1 / populacao1; // Cálculo do PIB per capita da primeira carta
 
 // Declaração das variáveis para a carta 2
 char estado2, codigo2[4], nome_cidade2[100];
 int populacao2, pontos_turisticos2;
 float area2, pib2, densi_pop2, pibpc2; // Acréscimo das variáveis densi_pop2 e pibpc2
-densi_pop2 = populacao2 / area2; // Cálculo da densidade populacional da segunda carta
-pibpc2 = pib2 / populacao2; // Cálculo do PIB per capita da segunda carta
 
 // Informações para a Carta 1
 
 printf("Cadastro da primeira carta:\n");
 printf("Informe o código da cidade (ex.: A01): ");
-scanf("%s", codigo1);
+scanf("%3s", codigo1);
 printf("População: ");
 scanf("%d", &populacao1);
 printf("Área (em km2): ");
@@ -29,14 +25,17 @@ printf("PIB (em bilhões): ");
 scanf("%f", &pib1);
 printf("Número de pontos turísticos: ");
 scanf("%d", &pontos_turisticos1);
-printf("Densidade Populacional: %.2f\n", densi_pop1); // Imprime a densidade populacional
-printf("PIB per capita: %.2f\n", pibpc1); // Imprime o PIB per capita
+
+// Cálculo da densidade populacional e PIB per capita da primeira carta
+  
+densi_pop1 = (float) populacao1 / area1;
+pibpc1 = pib1 / populacao1;
 
 // Informações para a Carta 2
 
 printf("Cadastro da segunda carta:\n");
 printf("Informe o código da cidade (ex.: B02): ");
-scanf("%s", codigo2);
+scanf("%3s", codigo2);
 printf("População: ");
 scanf("%d", &populacao2);
 printf("Área (em km2): ");
@@ -45,9 +44,12 @@ printf("PIB (em bilhões): ");
 scanf("%f", &pib2);
 printf("Número de pontos turísticos: ");
 scanf("%d", &pontos_turisticos2);
-printf("Densidade Populacional: %.2f\n", densi_pop2); // Imprime a densidade populacional
-printf("PIB per capita: %.2f\n", pibpc2) // Imprime o PIB per capita
 
+// Cálculo da densidade populacional e PIB per capita da segunda carta  
+
+densi_pop2 = (float) populacao2 / area2;
+pibpc2 = pib2 / populacao2;
+  
 // Exibição das cartas
 
 printf("\n--- Dados da Primeira Carta ---\n");
@@ -56,6 +58,8 @@ printf("População: %d\n", populacao1);
 printf("Área: %.2f km2\n", area1);
 printf("PIB: %.2f bilhões\n", pib1);
 printf("Número de pontos turísticos: %d\n", pontos_turisticos1);
+printf("Densidade Populacional: %.2f\n", densi_pop1); // Imprime a densidade populacional
+printf("PIB per capita: %.2f\n", pibpc1); // Imprime o PIB per capita
 
 printf("\n--- Dados da Segunda Carta ---\n");
 printf("Código: %s\n", codigo2);
@@ -63,6 +67,9 @@ printf("População: %d\n", populacao2);
 printf("Área: %.2f km2\n", area2);
 printf("PIB: %.2f bilhões\n", pib2);
 printf("Número de pontos turísticos: %d\n", pontos_turisticos2);
+printf("Densidade Populacional: %.2f\n", densi_pop2); // Imprime a densidade populacional
+printf("PIB per capita: %.2f\n", pibpc2); // Imprime o PIB per capita
+
 
 return 0;
 
