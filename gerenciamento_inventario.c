3596bc7b9ab65007b34213776081b231f5063544
Sávio Miranda <saviomrocha7@gmail.com>
2025-09-24 18:31:11 +0000

    adição da impressão das variáveis

--- gerenciamento_inventario.c
+++ gerenciamento_inventario.c
@@ -6,7 +6,7 @@ int main() {
   char produtoB[30] = "Produto B";
 
 unsigned int estoqueA = 1000;
-unsigend int estoqueB = 2000;
+unsigned int estoqueB = 2000;
 
 float valorA = 10.50;
 float valorB = 20.40;
@@ -16,3 +16,7 @@ unsigned int estoqueMinimoB = 2500;
 
 double valorTotalA;
 double valorTotalB;
+
+printf("Produto %s tem estoque %u e o valor unitário é %f\n", produtoA, estoqueA, valorA);
+printf("Produto %s tem estoque %u e o valor unitário é %f\n", produtoB, estoqueB, valorB);
+
