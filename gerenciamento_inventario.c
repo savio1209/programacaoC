2037408965151d983b84df9deaf47a15c51af480
Sávio Miranda <saviomrocha7@gmail.com>
2025-09-24 06:12:27 -0300

    Update gerenciamento_inventario.c
    declarei as variáveis

--- gerenciamento_inventario.c
+++ gerenciamento_inventario.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+
+  char produtoA[30] = "Produto A";
+  char produtoB[30] = "Produto B";
+
+unsigned int estoqueA = 1000;
+unsigend int estoqueB = 2000;
+
+float valorA = 10.50;
+float valorB = 20.40;
+
+unsigned int estoqueMinimoA = 500;
+unsigned int estoqueMinimoB = 2500;
+
+double valorTotalA;
+double valorTotalB;
