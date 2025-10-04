be4d5d4d893e78f1dfb01486a4db5000d6e3844a
Sávio Miranda <saviomrocha7@gmail.com>
2025-10-04 05:02:30 +0000

    estrutura simples com "if"

---
+++ estrututra_simples_if.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+    int idade1, idade2;
+
+    idade1 = 26;
+    idade2 = 17;
+
+    if (idade1 > idade2) {
+        printf("Fulano é mais velho que ciclano");
+    }
+
+}
