6a149d0a640edf4b67a297b3ad0e515bf0693384
Sávio Miranda <saviomrocha7@gmail.com>
2025-10-04 05:29:38 +0000

    estrutura composta com if e else

---
+++ praticas-de-codigo/estrutura_composta_if_else.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+
+    int idade1 = 19;
+    int idade2 = 18;
+
+    if (idade1 > idade2) {
+        // Se a condição for verdadeira:
+        printf("A pessoa é maior de idade\n");
+        // Se a condição for verdadeira:
+    } else {
+        printf("A pessoa é menor de idade\n");
+    }
+}
