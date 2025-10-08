97730f0fff3340fbe67f24017c9330beb7b0fb14
Sávio Miranda <saviomrocha7@gmail.com>
2025-10-08 04:00:48 +0000

    entrada dos dados

---
+++ praticas-de-codigo/pratica1_tema3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    float temperatura, umidade;
+    unsigned int estoque;
+
+    printf("Entre com a temperatura: \n");
+    scanf("%f", &temperatura);
+
+    printf("Entre com a umidade: \n");
+    scanf("%f", &umidade);
+
+    printf("Entre com o estoque: \n");
+    scanf("%u", &estoque);
+}
