f1c0e5667bdee8aa9a9cc47f4fa879242adb7ddc
Sávio Miranda <saviomrocha7@gmail.com>
2025-10-08 22:13:44 +0000

    exercício de estrutura de decisão aninhada

--- exercicios_tema_3/ex4_nivel_aventureiro.c
+++ exercicios_tema_3/ex4_nivel_aventureiro.c
@@ -15,4 +15,17 @@ scanf("%d", &idade);
 printf("Digite a sua renda mensal: \n");
 scanf("%f", &renda);
 
+if (idade <= 18 || idade >= 60)
+{
+    if (renda < 2000)
+    {
+    printf("Você tem direito ao desconto!\n");
+    } else {
+        printf("Você não tem direito ao desconto devido à renda!\n");
+    }
+    printf("Aceito com relação a idade.\n");
+} else {
+    printf("Você não atende aos critérios devido a idade.\n");
+}
+
 }
