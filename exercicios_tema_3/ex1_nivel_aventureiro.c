38d8d78216b26f14b1f3b02f17c6276fe4afa9a8
Sávio Miranda <saviomrocha7@gmail.com>
2025-10-08 13:17:20 +0000

    alteração no valor da variável idade e correção na pontuação

--- exercicios_tema_3/ex1_nivel_aventureiro.c
+++ exercicios_tema_3/ex1_nivel_aventureiro.c
@@ -2,7 +2,7 @@
 
 int main(){
 
-    int idade = 17;
+    int idade = 18;
     float altura = 1.75;
 
     // idade >= 18 => Falso
@@ -12,8 +12,8 @@ int main(){
     // Falso && Verdadeira => Falso
 
     if (idade >= 18 && idade <= 30 && altura > 1.70) {
-        printf("Você está na faixa etária e tem a altura adequada\n");
+        printf("Você está na faixa etária e tem a altura adequada.\n");
     } else {
-        printf("Você não atende aos critérios\n");
+        printf("Você não atende aos critérios.\n");
     }
 }
