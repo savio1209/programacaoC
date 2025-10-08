1aba0d62005d9cd20fdbdfc1e1bb513f7e2ea0ec
Sávio Miranda <saviomrocha7@gmail.com>
2025-10-08 04:27:53 +0000

    substituição da comparação de atributos pela estrutura composta

---
+++ desafio-super-trunfo/desafio_super_trunf_paises_2.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+
+int main() {
+
+// Declaração das variáveis para a carta 1 
+char estado1[100], codigo1[4], nome_cidade1[100];
+int pontos_turisticos1;
+unsigned long int populacao1;
+float area1, pib1, densi_pop1, pibpc1, superpoder1;
+
+// Declaração das variáveis para a carta 2
+char estado2[100], codigo2[4], nome_cidade2[100];
+int pontos_turisticos2;
+unsigned long int populacao2;
+float area2, pib2, densi_pop2, pibpc2, superpoder2;
+
+// Informações para a Carta 1
+
+printf("Cadastro da primeira carta:\n");
+printf("Informe o nome do estado: \n");
+scanf("%s", estado1);
+printf("Informe o código da cidade (ex.: A01): \n");
+scanf("%3s", codigo1);
+printf("Nome da cidade: \n");
+scanf("%s", nome_cidade1);
+printf("População: \n");
+scanf("%lu", &populacao1);
+printf("Área (em km2): \n");
+scanf("%f", &area1);
+printf("PIB (em bilhões): \n");
+scanf("%f", &pib1);
+printf("Número de pontos turísticos: \n");
+scanf("%d", &pontos_turisticos1);
+
+// Cálculo da densidade populacional e PIB per capita da primeira carta
+densi_pop1 = (float) populacao1 / area1;
+pibpc1 = pib1 / populacao1;
+
+// Cálculo dos atributos que juntos formam o Super Poder da primeira carta
+superpoder1 = (float) populacao1 + area1 + pib1 + pontos_turisticos1 + pibpc1 + (1.0 /densi_pop1);
+
+// Informações para a Carta 2
+
+printf("Cadastro da segunda carta:\n");
+printf("Informe o nome do estado: \n");
+scanf("%s", estado2);
+printf("Informe o código da cidade (ex.: B02): \n");
+scanf("%3s", codigo2);
+printf("Nome da cidade: \n");
+scanf("%s", nome_cidade2);
+printf("População: \n");
+scanf("%lu", &populacao2);
+printf("Área (em km2): \n");
+scanf("%f", &area2);
+printf("PIB (em bilhões): \n");
+scanf("%f", &pib2);
+printf("Número de pontos turísticos: \n");
+scanf("%d", &pontos_turisticos2);
+
+// Cálculo da densidade populacional e PIB per capita da segunda carta  
+
+densi_pop2 = (float) populacao2 / area2;
+pibpc2 = pib2 / populacao2;
+
+// Cálculo dos atributos que juntos formam o Super Poder da segunda carta
+superpoder2 = (float) populacao2 + area2 + pib2 + pontos_turisticos2 + pibpc2 + (1.0 /densi_pop2);
+  
+// Exibição dos dados da carta 1
+
+printf("\n--- Dados da Primeira Carta ---\n");
+printf("Código: %s\n", codigo1);
+printf("População: %lu\n", populacao1);
+printf("Área: %.2f km2\n", area1);
+printf("PIB: %.2f bilhões\n", pib1);
+printf("Número de pontos turísticos: %d\n", pontos_turisticos1);
+printf("Densidade Populacional: %.2f\n", densi_pop1);
+printf("PIB per capita: %.2f\n", pibpc1);
+printf("Super Poder da primeira carta: %f\n", superpoder1);
+
+// Exibição dos dados da carta 2
+
+printf("\n--- Dados da Segunda Carta ---\n");
+printf("Código: %s\n", codigo2);
+printf("População: %lu\n", populacao2);
+printf("Área: %.2f km2\n", area2);
+printf("PIB: %.2f bilhões\n", pib2);
+printf("Número de pontos turísticos: %d\n", pontos_turisticos2);
+printf("Densidade Populacional: %.2f\n", densi_pop2);
+printf("PIB per capita: %.2f\n", pibpc2);
+printf("Super Poder da segunda carta: %f\n", superpoder2);
+
+// Comparação do atributo "superpoder" utilizando operadores relacionais com estrutura composta
+
+if (superpoder1 > superpoder2){
+    printf("A carta 1 venceu! Parabéns!\n");
+} else {
+    printf ("A carta 2 venceu! Parabéns!\n");
+}
+
+return 0;
+
+}
