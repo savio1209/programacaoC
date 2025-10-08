937ddddf257b22282c1bef3209d0999ee3c945d9
Sávio Miranda <saviomrocha7@gmail.com>
2025-10-08 13:32:40 +0000

    adição da estrutura de condição encadeada utilizando operadores lógicos

--- exercicios_tema_3/ex2_nivel_aventureiro.c
+++ exercicios_tema_3/ex2_nivel_aventureiro.c
@@ -7,6 +7,19 @@ int idade;
 printf("Digite a sua idade: \n");
 scanf("%d", &idade);
 
-//
+//Criança < 12
+//Adolescente 12 <= x < 18
+//Adulto 18 <= x < 60
+//Idoso >= 60
+
+if (idade < 12) {
+    printf("Você é uma criança.\n");
+} else if (idade >= 12 && idade < 18) {
+    printf("Você é um adolescente.\n");
+} else if (idade >= 18 && idade <60) {
+    printf("Você é um adulto.\n");
+} else {
+    printf("Você é um idoso.\n");
+}
 
 }
