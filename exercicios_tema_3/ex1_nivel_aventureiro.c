2e6bd1c07c53e1e8ac3265741dfe953292c92a59
Sávio Miranda <saviomrocha7@gmail.com>
2025-10-08 06:03:10 +0000

    adição dos comentários que exemplificam a lógica

--- exercicios_tema_3/ex1_nivel_aventureiro.c
+++ exercicios_tema_3/ex1_nivel_aventureiro.c
@@ -5,6 +5,12 @@ int main(){
     int idade = 20;
     float altura = 1.75;
 
+    // idade >= 18 => Verdadeira
+    // idade <= 30 => Verdadeira
+    // Verdadeira && Verdadeira
+    // Verdadeira && altura > 1.70
+    // Verdadeira && Verdadeira => Verdadeira
+
     if (idade >= 18 && idade <= 30 && altura > 1.70) {
         printf("Você está na faixa etária e tem a altura adequada\n");
     } else {
