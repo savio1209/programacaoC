44b533bd110d3e511532c64800e075c2b9a61f50
Sávio Miranda <saviomrocha7@gmail.com>
2025-10-08 04:07:26 +0000

    teste das variáveis

--- praticas-de-codigo/pratica1_tema3.c
+++ praticas-de-codigo/pratica1_tema3.c
@@ -3,7 +3,7 @@
 int main(){
 
     float temperatura, umidade;
-    unsigned int estoque;
+    unsigned int estoque, estoqueMinimo = 1000;
 
     printf("Entre com a temperatura: \n");
     scanf("%f", &temperatura);
@@ -13,4 +13,23 @@ int main(){
 
     printf("Entre com o estoque: \n");
     scanf("%u", &estoque);
+
+    if (temperatura > 30){
+        printf("A temperatura está alta.\n");
+    } else {
+        printf("A temperatura está dentro dos parâmetros.");
+
+    }
+
+    if (umidade > 50){
+        printf("A umidade está elevada.\n";)
+    } else {
+        printf("A temperatura está dentro dos parâmetros.\n");
+    }
+
+    if (estoque < estoqueMinimo){
+        printf("Estoque abaixo do mínimo.\n");
+    } else {
+        printf("Estoque normal.\n");
+    }
 }
