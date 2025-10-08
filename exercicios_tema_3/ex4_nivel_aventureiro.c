9d195e7b9dccf85f978f65d121d66bcdc6ed09b2
Sávio Miranda <saviomrocha7@gmail.com>
2025-10-08 21:35:38 +0000

    declaração das variáveis e exibição dos dados

---
+++ exercicios_tema_3/ex4_nivel_aventureiro.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade;
+    float renda; 
+
+/* Programa que verifica se uma está qualificada para um
+desconto especial com base na idade e na renda mensal. 
+A pessoa deve ter mais de 60 anos ou menos de 18 anos e ter
+uma renda mensal abaixo de 2000 */
+
+printf("Digite a sua idade: \n");
+scanf("%d", &idade);
+printf("Digite a sua renda mensal: \n");
+scanf("%f", &renda);
+
+}
