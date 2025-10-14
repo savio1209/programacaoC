8e3e1aa583d49db4e223b81ee3ee01c485b4f00e
Sávio Miranda <saviomrocha7@gmail.com>
2025-10-14 07:29:41 +0000

    exemplo de uso do switch em c

--- Tema_3/Nível_Aventureiro/ex7_nivel_aventureiro.c
+++ Tema_3/Nível_Aventureiro/ex7_nivel_aventureiro.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main(){
+char variavel;
+
+printf("Digite um valor: \n");
+scanf("%c", &variavel);
+
+switch (variavel) {
+    case 'a':
+    // Código a ser executado se variavel == a
+    printf("Código a ser executado se variável == a\n");
+    printf("Teste do case 'a'\n");
+    break;
+    case 'b':
+    // Código a ser executado se variavel == b
+    printf("Código a ser executado se variavel == b\n");
+    break;
+    default:
+    // Código a ser executado se nenhum dos casos acima for verdadeiro
+    printf("Código a ser executado se variavel não for a ou b\n");
+
+}
+}
