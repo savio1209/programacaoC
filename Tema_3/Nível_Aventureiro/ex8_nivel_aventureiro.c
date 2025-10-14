7b4cdb652bc47258433f9d01a44135727f6bf617
Sávio Miranda <saviomrocha7@gmail.com>
2025-10-14 07:35:45 +0000

    declaração da variavel opcao e leitura dos dados

---
+++ Tema_3/Nível_Aventureiro/ex8_nivel_aventureiro.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int opcao;
+
+    printf("Escolha uma opção:\n");
+    printf("1. Verificar saldo\n");
+    printf("2. Fazer depósito\n");
+    printf("3. Fazer saque\n");
+    scanf("%d", &opcao);
+}
