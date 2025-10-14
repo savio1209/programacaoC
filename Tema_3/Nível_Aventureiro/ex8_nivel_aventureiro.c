bec26abd7976bfb365c49686b72bd610fde65e32
Sávio Miranda <saviomrocha7@gmail.com>
2025-10-14 07:53:16 +0000

    exibição dos dados

--- Tema_3/Nível_Aventureiro/ex8_nivel_aventureiro.c
+++ Tema_3/Nível_Aventureiro/ex8_nivel_aventureiro.c
@@ -2,10 +2,21 @@
 
 int main() {
     int opcao;
+    float saldo = 1000.00;
 
     printf("Escolha uma opção:\n");
     printf("1. Verificar saldo\n");
     printf("2. Fazer depósito\n");
     printf("3. Fazer saque\n");
     scanf("%d", &opcao);
+
+    switch (opcao) {
+        case 1:
+        printf("O seu saldo é: R$%f\n");
+        break;
+        case 2:
+        printf("Digite o banco que você deseja depositar:\n");
+        printf("Digite a agência que você deseja depositar:\n");
+        printf("Digite a conta que você deseja ")
+    }
 }
