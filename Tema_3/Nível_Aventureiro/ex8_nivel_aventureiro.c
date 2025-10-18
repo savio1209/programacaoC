1e3b89ac9cb2892716df8588ba965001de91805b
Sávio Miranda <saviomrocha7@gmail.com>
2025-10-18 06:15:29 +0000

    adição das variáveis agencia e conta

--- Tema_3/Nível_Aventureiro/ex8_nivel_aventureiro.c
+++ Tema_3/Nível_Aventureiro/ex8_nivel_aventureiro.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    int opcao;
+    int opcao, agencia, conta;
     float saldo = 1000.00;
+    char banco[20];
 
     printf("Escolha uma opção:\n");
     printf("1. Verificar saldo\n");
@@ -12,16 +13,22 @@ int main() {
 
     switch (opcao) {
         case 1:
-        printf("O seu saldo é: R$%f\n");
+        printf("O seu saldo é: R$%.2f\n", saldo);
         break;
+
         case 2:
         printf("Digite o banco que você deseja depositar:\n");
+        scanf(" %c", &banco);
         printf("Digite a agência que você deseja depositar:\n");
+        scanf(" %d", &agencia);
         printf("Digite a conta que você deseja depositar:\n");
+        scanf(" %d", &conta);
         break;
+
         case 3:
         printf("Digite o valor a sacar:\n");
         break;
+
         default:
         printf("Opção inválida!\n");
     }
