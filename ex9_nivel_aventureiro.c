2389b99440f142e811d3a5dbdb4b17eed1d98e76
Sávio Miranda <saviomrocha7@gmail.com>
2025-10-19 07:37:21 +0000

    estrutura switch pra testar a variável opcao

--- ex9_nivel_aventureiro.c
+++ ex9_nivel_aventureiro.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
+// Declaração da variável opcao.
 int main() {
 int opcao;
 
+// Menu interativo com 3 opções.
 printf("Menu Principal\n");
 printf("1. Iniciar Jogo\n");
 printf("2. Ver Regras\n");
@@ -10,7 +12,25 @@ printf("3. Sair\n");
 printf("Escolha uma opcao: ");
 scanf("%d", &opcao);
 
+// Estrutura switch
+switch (opcao)
+{
+case 1:
+    printf("Execução do Jogo\n");
+    break;
 
+case 2:
+    printf("As regras são: \n");
+    break;
+
+case 3:
+    printf("Saindo do jogo...\n");
+    break;
+
+default:
+    printf("Opção inválida.\n")
+    break;
+}
 
 
 }
