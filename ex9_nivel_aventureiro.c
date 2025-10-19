a592235fcc887532e58fc9cc530006192af7e6a9
Sávio Miranda <saviomrocha7@gmail.com>
2025-10-19 07:27:01 +0000

    declaração da variável e do menu do jogo

---
+++ ex9_nivel_aventureiro.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+int opcao;
+
+printf("Menu Principal\n");
+printf("1. Iniciar Jogo\n");
+printf("2. Ver Regras\n");
+printf("3. Sair\n");
+printf("Escolha uma opcao: ");
+scanf("%d", &opcao);
+
+
+
+
+}
